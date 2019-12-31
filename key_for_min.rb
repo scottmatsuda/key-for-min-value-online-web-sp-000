e7ba4daf8776fbd7c3f8bc2265545441e7f7428a
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-31 18:06:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   array = name_hash.collect do |key, value|
     {key => value}
   end
-  initial = array[0][1]
+  initial = array[0]
   p initial
   # array.each do |key, value|
   #   if value < initial
