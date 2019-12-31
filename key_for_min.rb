69538b8936d1a1db4cdd0982ed00630042be93b7
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-31 17:40:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,11 @@
 def key_for_min_value(name_hash)
   # intital_value = name_hash[0]
   # puts initial_value
-  name_hash.each do |key, value|
-    if value < intital_value
-      initial value = value
-    end
-  end
-  return intial_value
+  puts name_hash[0]
+  # name_hash.each do |key, value|
+  #   if value < intital_value
+  #     initial value = value
+  #   end
+  # end
+  # return intial_value
 end
